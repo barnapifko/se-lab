36e44e27f81e1501ae2767bfba401c451b5f3636
Pifkó <pifko.barna@gmail.com>
2023-10-11 15:46:29 +0200

    expception issue fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,7 +31,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      //new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
